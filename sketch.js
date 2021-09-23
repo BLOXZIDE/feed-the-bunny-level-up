ce94dc9ae7c16eeb5687a803c3ac5f48019d29fc
BLOXZIDE <82806724+BLOXZIDE@users.noreply.github.com>
2021-09-23 17:48:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,184 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+let engine;
+let world;
+var rope,fruit,ground;
+var fruit_con;
+var fruit_con_2;
+
+var bg_img;
+var food;
+var rabbit;
+
+var button;
+var bunny;
+var blink,eat,sad;
+var food2
+function preload()
+{
+  bg_img = loadImage('background.png');
+  food = loadImage('melon.png');
+  food2 = loadImage('MELONBUBBLE.png')
+  rabbit = loadImage('Rabbit-01.png');;
+  blink = loadAnimation("blink_1.png","blink_2.png","blink_3.png");
+  eat = loadAnimation("eat_0.png" , "eat_1.png","eat_2.png","eat_3.png","eat_4.png");
+  sad = loadAnimation("sad_1.png","sad_2.png","sad_3.png");
+  bubleo = loadAnimation("bubble.png")
+  blink.playing = true;
+  eat.playing = true;
+  sad.playing = true;
+  sad.looping= false;
+  eat.looping = false; 
+}
+
+function setup() {
+  createCanvas(500,700);
+  frameRate(80);
+
+  engine = Engine.create();
+  world = engine.world;
+  
+  button = createImg('cut_btn.png');
+  button.position(50,360);
+  button.size(50,50);
+  button.mouseClicked(drop);
+  
+  
+  button2 = createImg('cut_btn.png');
+  button2.position(210,250);
+  button2.size(50,50);
+  button2.mouseClicked(drop2);
+  
+  blink.frameDelay = 20;
+  eat.frameDelay = 20;
+
+  bunny = createSprite(330,100,100,100);
+  bunny.scale = 0.2;
+
+  bubble = createSprite(340,510,50,50)
+  bubble.addAnimation("2",food2)
+  bubble.addAnimation("b",bubleo)
+  bubble.changeAnimation("b",bubleo)
+  bubble.scale = 0.1
+
+  bunny.addAnimation('blinking',blink);
+  bunny.addAnimation('eating',eat);
+  bunny.addAnimation('crying',sad);
+  bunny.changeAnimation('blinking');
+  
+  rope = new Rope(7,{x:70,y:360});
+  rope2 = new Rope(8,{x:250,y:250});
+  ground = new Ground(320,170,100,20);
+  
+  fruit = Bodies.circle(70,700,20);
+  Matter.Composite.add(rope.body,fruit);
+  
+
+  fruit_con = new Link(rope,fruit);
+  fruit_con2 = new Link(rope2,fruit)
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+  imageMode(CENTER);
+  
+}
+
+  function draw() 
+  {
+    background(51);
+    image(bg_img,width/2,height/2,490,690);
+
+
+  rope.show();
+  Engine.update(engine);
+  ground.show();
+ rope2.show()
+
+  if (fruit != null)
+  {
+    image(food,fruit.position.x,fruit.position.y,70,70);
+  }
+
+
+  if(collide(fruit,bunny) === true)
+  {
+    bunny.changeAnimation("eating")
+  }
+  if(collide(fruit,ground.body) === true)
+  {
+    bunny.changeAnimation("crying")
+  }
+  
+  if(collide0(fruit,bubble) === true)
+  {
+    bubble.changeAnimation("2",food2)
+    bubble.velocityY = -5
+  }
+  if(bubble.isTouching(bunny))
+  {
+    bubble.visible = false
+    bunny.changeAnimation("eating")
+  }
+   drawSprites();
+}
+
+function drop()
+{
+  rope.break();
+  fruit_con.detach();
+  fruit_con = null; 
+}
+
+function drop2()
+{
+  rope2.break();
+  fruit_con2.detach();
+  fruit_con2 = null; 
+}
+
+
+function collide(body,sprite)
+{
+ if(body != null)
+ {
+   var d = dist(body.position.x,body.position.y,sprite.position.x,sprite.position.y)
+   
+ if(d <= 80)
+ {
+   World.remove(engine.world,fruit)
+  fruit = null
+   return true
+ }
+ else{
+   return false
+ }
+  }
+ 
+}
+
+function collide0(body,sprite)
+{
+ if(body != null)
+ {
+   var d = dist(body.position.x,body.position.y,sprite.position.x,sprite.position.y)
+   
+ if(d <= 50)
+ {
+  World.remove(engine.world,fruit)
+  fruit = null
+
+   return true
+ }
+ else{
+  return false
+ }
+  }
+ 
+}
+
